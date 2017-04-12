bcb9cecbaede7cbf379e4df2684a4cb5e030aca5
yuritx <texeira.yuri@hotmail.com>
2017-04-11 21:12:08 -0300

    Update main.java

--- main.java
+++ main.java
@@ -3,29 +3,46 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		
-	Scanner scanner = new Scanner(new FileReader("C:\\Users\\yuri\\Desktop\\easy.txt"))
-        .useDelimiter("");
-	int numero = 0;
-while (scanner.hasNext()) {
+	ArrayList <Pessoa> pessoas = new ArrayList<Pessoa>();  	
+	Scanner scanner = new Scanner(new FileReader("C:\\Users\\yuri\\Desktop\\paths_d.txt"))
+    .useDelimiter("\\n"); 
+	int metro = 0;
+	while (scanner.hasNextLine()) {
+		Pessoa pes = null;
+		String g = scanner.nextLine();
+		Scanner scan = new Scanner(g).useDelimiter("[, ) \\t]");
+		int aux = 0;
+		while (scan.hasNext()){
+			switch(aux){
+				case 0:
+					String x = scan.next();
+					int frames = Integer.parseInt(x.replaceAll("[^0-9]", ""));
+					pes = new Pessoa(pessoas.size(), frames, metro);
+					aux =1;
+				case 1:
+					String z = scan.next();
+					int valorX= Integer.parseInt(z.replaceAll("[^0-9]", ""));
+					z = scan.next();
+					int valorY= Integer.parseInt(z.replaceAll("[^0-9]", ""));
+					pes.addFrame(valorX, valorY);
+				case 2:
+					scan.next();
+					aux = 1;
+			}	
+		}
+		scan.close();
+		pessoas.add(pes);
+	}
 
-if( scanner.hasNextInt()){
-	numero = scanner.nextInt(); 
-	System.out.println(numero);}
-	else{ 
-		scanner.next();}
-
-
-
-}
+	scanner.close();
 	}
 		
 }	
-		
 		
